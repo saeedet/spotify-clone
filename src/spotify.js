0c3521c452b36244c1ae1b6569d0227dc804b597
Saeed <saeed_et90@yahoo.com>
2021-08-12 11:52:08 +1000

    CI and redirect url fixed

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-nu-two.vercel.app/";
 
 const { CLIENT_ID } = process.env;
 
